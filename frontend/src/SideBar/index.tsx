59bc9d4a056b5213fea5d31599ccecc72d18e0e1
CiroLee <ciromail@126.com>
2023-04-10 01:13:02 +0800

    feat: init

--- frontend/src/SideBar/index.tsx
+++ frontend/src/SideBar/index.tsx
@@ -4,7 +4,18 @@ import style from './index.module.scss';
 const cn = classnames.bind(style);
 const SideBar: FC = () => {
   const [collapse, setCollapse] = useState(false);
-  return <div className={cn('sidebar', { collapse })}>sidebar</div>;
+  return (
+    <div className={cn('sidebar', { collapse })}>
+      <div>
+        <div className={cn('sidebar__title')}>
+          <div className="flex item-center">
+            <i className={cn('sidebar__title-icon')}></i>
+            <span className="text-[18px] ml-2">ChatY</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import './styles/theme.scss';
+import 'remixicon/fonts/remixicon.css';
 import './styles/reset.css';
 
 const container = document.getElementById('root');
